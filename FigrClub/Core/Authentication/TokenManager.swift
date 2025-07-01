2cc98c0bd882f1f487237a9c50cea69cd5b8cec4
EmerBV <emersmc@hotmail.com>
2025-07-01 08:16:07 +0200

    fixing errors

--- FigrClub/Core/Authentication/TokenManager.swift
+++ FigrClub/Core/Authentication/TokenManager.swift
@@ -244,27 +244,3 @@ struct RefreshTokenRequest: Codable {
     let refreshToken: String
 }
 
-// MARK: - Combine Publisher Extension
-extension Publisher {
-    func async() async throws -> Output {
-        try await withCheckedThrowingContinuation { continuation in
-            var cancellable: AnyCancellable?
-            cancellable = first()
-                .sink(
-                    receiveCompletion: { completion in
-                        switch completion {
-                        case .finished:
-                            break
-                        case .failure(let error):
-                            continuation.resume(throwing: error)
-                        }
-                        cancellable?.cancel()
-                    },
-                    receiveValue: { value in
-                        continuation.resume(returning: value)
-                        cancellable?.cancel()
-                    }
-                )
-        }
-    }
-}
